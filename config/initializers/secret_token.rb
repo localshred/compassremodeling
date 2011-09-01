79c86e7629afb59dea6a8956fe6ba0e32a1f4a2e
BJ Neilsen <bj.neilsen@gmail.com>
2011-08-31 22:39:03 -0600

    Created default refinerycms app, initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compass::Application.config.secret_token = 'e66f93c412f12e29da2323dcc720ff0e10c17189f0a70704f8dd9a05622f04bb9b8fe55d4baa29709cbe7dc8afe34657f7a097853074e09f200530a9a6e37d64'
